37b3334771def43596580029f57a4be6320e71d9
Basel Farag <bfarag@wolfgang.com>
2015-08-27 11:16:02 -0600

    first commit

---
+++ Amadeus SquirrelApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Amadeus SquirrelApp
+//
+//  Created by Basel Farag on 8/27/15.
+//  Copyright (c) 2015 Basel Farag. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
