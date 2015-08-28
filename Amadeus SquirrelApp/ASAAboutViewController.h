51092055f0fd6d397a2580f4e3cdc6b674550d05
Basel Farag <bfarag@wolfgang.com>
2015-08-28 08:13:30 -0600

    Added several pods. Set up tableview and data model

---
+++ Amadeus SquirrelApp/ASAAboutViewController.h
@@ -0,0 +1,13 @@
+//
+//  ASAAboutViewController.h
+//  Amadeus SquirrelApp
+//
+//  Created by Basel Farag on 8/27/15.
+//  Copyright (c) 2015 Basel Farag. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ASAAboutViewController : UIViewController
+
+@end
---
+++ Amadeus SquirrelApp/ASAAboutViewController.m
@@ -0,0 +1,37 @@
+//
+//  ASAAboutViewController.m
+//  Amadeus SquirrelApp
+//
+//  Created by Basel Farag on 8/27/15.
+//  Copyright (c) 2015 Basel Farag. All rights reserved.
+//
+
+#import "ASAAboutViewController.h"
+
+@interface ASAAboutViewController ()
+
+@end
+
+@implementation ASAAboutViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
