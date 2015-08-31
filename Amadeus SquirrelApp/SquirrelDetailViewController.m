bc5f6d7d2b0470005fa21f5fdf3c809ca65f38cd
Basel Farag <bfarag@wolfgang.com>
2015-08-31 10:50:14 -0600

    Cleaned up

---
+++ Amadeus SquirrelApp/SquirrelDetailViewController.m
@@ -0,0 +1,37 @@
+//
+//  SquirrelDetailViewController.m
+//  Amadeus SquirrelApp
+//
+//  Created by Basel Farag on 8/28/15.
+//  Copyright (c) 2015 Basel Farag. All rights reserved.
+//
+
+#import "SquirrelDetailViewController.h"
+
+@interface SquirrelDetailViewController ()
+
+@end
+
+@implementation SquirrelDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
